5447c30a0e218360ece5d0cdece6dffd555f0d79
Igor Agapie <igoragapie@gmail.com>
2026-07-28 12:02:53 +0200

    refactor: simplify dotenv loading logic in env.go

--- env.go
+++ env.go
@@ -9,9 +9,8 @@ import (
 const envDotenv = "DOTENV_PATH"
 
 func init() {
+	_ = godotenv.Load()
 	if dotenv, ok := os.LookupEnv(envDotenv); ok {
 		_ = godotenv.Load(dotenv)
-	} else {
-		_ = godotenv.Load()
 	}
 }
